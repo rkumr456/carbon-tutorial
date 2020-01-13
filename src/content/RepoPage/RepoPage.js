fd1c6b25f506a1273d969710cd2cce1541a09798
rkumr456 <rkumr456@in.ibm.com>
2020-01-13 16:28:11 +0530

    feat(tutorial): complete step 1

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 const RepoPage = () => {
-  return <div>REPO PAGE</div>;
+  return (
+    <div>
+      <h1>Hello Mr Rohit Welcome to Repo Page</h1>
+    </div>
+  );
 };
 export default RepoPage;
